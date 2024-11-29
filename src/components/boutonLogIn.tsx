2437d8e40eb1202fdcc7b0c0fa874e80074539a5
cindy <cindy.bragdon@gmail.com>
2024-11-29 18:46:23 -0500

    navigate SignIn LogIn

--- src/components/boutonLogIn.tsx
+++ src/components/boutonLogIn.tsx
@@ -1,13 +1,19 @@
 import React from "react";
-
+import { useNavigate } from 'react-router-dom';
 
 
 const BoutonLogIn = () => {
 
+const navigate = useNavigate();
+
+const handleNavigate = (path: string) => {
+navigate(path);
+};
+
     return (
 
         <div >
-            <button className="custom-button">Log In</button>
+            <button className="custom-button"onClick={() => handleNavigate('/login')}>Login</button>
         </div>
     );
     };
--- src/components/boutonSignIn.tsx
+++ src/components/boutonSignIn.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+
 
 const BoutonSignIn = () => {
+
+const navigate = useNavigate();
+
+const handleNavigate = (path: string) => {
+    navigate(path);
+};
 
 return (
 
     <div >
-        <button className="custom-button">Sign In</button>
+        <button className="custom-button"onClick={() => handleNavigate('/signin')}>Sign In</button>
     </div>
 );
 };
