251be7db07a6dd0ac4581ea56f9d4044a88038be
cindy <cindy.bragdon@gmail.com>
2024-11-30 17:45:11 -0500

    chart.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Home from "./pages/Home.tsx";
 import Picks from "./pages/Picks.tsx";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const mockData = [
   { team: "Lakers", odds: "+120", points: 102, spread: -4.5 },
@@ -12,8 +12,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/picks" element={<Picks data={mockData} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/picks" element={<Picks data={mockData} />} />
       </Routes>
     </BrowserRouter>
   );
