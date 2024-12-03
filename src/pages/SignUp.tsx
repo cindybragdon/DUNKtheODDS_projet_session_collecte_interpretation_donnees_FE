34dbd6010aa84fbc36807d6735e2f5818f19a89b
cindy <cindy.bragdon@gmail.com>
2024-12-03 12:41:25 -0500

    rounded corners

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -66,7 +66,7 @@ const SignUpPage = () => {
                   id="confirmPassword"
                   autoComplete="current-password"
                   required
-                  className="rounded-lg block w-full bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
+                  className=" rounded block w-full bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm"
                 />
               </div>
             </div>
